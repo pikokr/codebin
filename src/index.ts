bd0b20ea222fb7db453a07e417311eff3f71bba4
pikokr <parangee9706@gmail.com>
2020-11-14 13:30:39 +0900

    clear

--- src/index.ts
+++
@@ -1,9 +0,0 @@
-import express from 'express'
-
-const app = express()
-
-app.use(express.json())
-
-app.use('/api', require('./routing').default)
-
-app.listen(3060)
--- src/routing/index.ts
+++
@@ -1,9 +0,0 @@
-import {Router} from "express";
-
-const router = Router()
-
-router.get('/', (req, res) => res.json({
-    hello: 'world'
-}))
-
-export default router
